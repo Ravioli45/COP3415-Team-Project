efce29e2c54d6b9e6a6328deee5d04aca385ff8f
Joseph Elibri <elibrij@gmail.com>
2024-04-17 23:16:54 -0400

    Added Utils.h and Utils.cpp added since I think std::swap is considered part of STL

---
+++ src/Utils.cpp
@@ -0,0 +1,10 @@
+#include "Utils.h"
+
+namespace utils{
+    template<typename T>
+    void swap(T& a, T& b){
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+}
---
+++ src/Utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+namespace utils{
+    template<typename T>
+    void swap(T& a, T& b);
+}
+
+#endif
